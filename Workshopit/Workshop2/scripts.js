7b8dc07690d3f828f96fa1ac824e99a72517016e
Joachim <JOP00060@laurea.fi>
2025-09-11 14:47:27 +0300

    ws02

---
+++ Workshopit/Workshop2/scripts.js
@@ -0,0 +1,8 @@
+// Tehtävä 1
+document.write(`"<b><em>\Jos minulla olisi yhdeksän tuntia puun kaatamiseen, käyttäisin ensimmäiset kuusi kirveen terottamiseen.\"</em></b><br><b/><b></br>-Abraham Lincoln`)
+// Tai
+document.getElementById("testi").innerHTML = `"Jos minulla olisi yhdeksän tuntia puun kaatamiseen, käyttäisin ensimmäiset kuusi kirveen terottamiseen."
+– Abraham Lincoln"`;
+//html filussa id="testi"
+
+// Tehtävä 2
